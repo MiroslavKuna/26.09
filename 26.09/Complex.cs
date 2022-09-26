8238eb178fcebce565c7298f173efd1fe7bbb6b2
MiroslavKuna <113085984+MiroslavKuna@users.noreply.github.com>
2022-09-26 09:05:36 +0200

    Add files via upload

---
+++ 26.09/Complex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26._09
+{
+    internal class Complex
+    {
+        private int real;
+        private int imagine;
+
+        public Complex(int real, int imagine)
+        {
+            Real = real;
+            Imagine = imagine;
+        }
+
+        public int Real { get => real; set => real = value; }
+        public int Imagine { get => imagine; set => imagine = value; }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.Real + b.Real, a.Imagine + b.Imagine);
+        }
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.Real - b.Real, a.Imagine - b.Imagine);
+        }
+        public override string ToString()
+        {
+            return Real+" + "+Imagine+"i";
+        }
+        public static bool operator ==(Complex a, Complex b) 
+        {
+            return a.Real == b.Real && a.Imagine == b.Imagine;
+        }
+        public static bool operator !=(Complex a, Complex b)
+        {
+            return !(a == b);
+        }
+    }
+}
---
+++ 26.09/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _26._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(5, 7);
+            Complex c2 = new Complex(5, 7);
+            Complex c3 = new Complex(5, 9);
+        }
+    }
+}
